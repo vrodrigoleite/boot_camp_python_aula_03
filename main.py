f3f5d3c6e2f5f77dc075422357fbc2449af9ceff
Victor Almeida <vrodrigoleite@gmail.com>
2024-09-25 10:26:11 -0300

    adicionando leitura de csv

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("teste")
+import csv
+
+path: str = "exemplo.csv"
+
+file_csv: list = []
+
+with open(path, mode="r", encoding="utf-8") as file:
+    read_csv = csv.DictReader(file)
+
+    for raw in read_csv:
+        file_csv.append(raw)
+
+print(file_csv)
